603d21440ea86c8c9e45dce04789fcaff18fb4ee
lyfu <501900442@qq.com>
2019-07-16 15:34:19 +0800

    first commit

---
+++ Project/app.d
@@ -0,0 +1,1 @@
+.\app.o: ..\User\app.c
---
+++ User/app.c
@@ -0,0 +1,56 @@
+
+
+
+
+void hardware_init(void)
+{
+	
+}
+
+//void dosomething1(void)
+//{
+//	
+//}
+
+
+//void dosomething2(void)
+//{
+//	
+//}
+
+//void dosomething3(void)
+//{
+//	
+//}
+
+
+void task(void *parg)
+{
+	/* 任务主体，无限循环且不能返回 */
+	for(;;)
+	{
+		/* 任务主体代码 */
+	}
+
+}
+
+int flag1 = 0;
+int flag2 = 0;
+int flag3 = 0;
+
+int main(void)
+{
+	/* 硬件初始化*/
+	hardware_init();
+	for(;;){
+//		/* 处理事件1*/
+//		dosomething1();
+//		
+//		/*处理事件2*/
+//		dosomething2();
+//		
+//		/*处理事件3*/
+//		dosomething3();
+	}
+
+}
